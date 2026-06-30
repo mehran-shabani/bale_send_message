499b69df2b7b9bdcb7ba45f5b2739b5b59cac200
Dr.hossein Shabani <shabanimehran@gmail.com>
2026-06-30 10:03:27 +0330

    Add sender app config

---
+++ bale_sender/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BaleSenderConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bale_sender"
+    verbose_name = "ارسال پیام بله"
